c1aa86badd19742bb541c68af89235ef5ff9e87d
ayushj03 <meayush434@gmail.com>
2026-01-10 18:36:23 +0530

    Added SignUp Login Pages

---
+++ Modules/signIn-signUp_Page/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Signup from './components/Signup'
+import SignIn from './components/SignIn'
+function App() {
+  return(
+    <>
+        <Signup/> 
+        {/* <SignIn/> */}
+    </>
+  )
+}
+
+export default App
